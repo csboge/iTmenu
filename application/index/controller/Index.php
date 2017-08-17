e66679fb2e96b36298275a07ab8286ea78c9292d
pengyangchao <474114736@qq.com>
2017-08-17 14:25:00 +0800

    init

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\index\controller;
 
+use app\index\model\User;
+
 class Index
 {
     public function index()
@@ -12,10 +14,8 @@ class Index
     public function login()
     {
 
-
-//        $this->success('新增成功', 'User/list');
-
         return view();
+
     }
 
 
@@ -25,4 +25,24 @@ class Index
         return view();
     }
 
+
+    /**
+     * @return \think\response\Json
+     * 检查用户名密码
+     */
+    public function check(){
+        $data = input('post.');
+        $code=404;
+        $msg='登录失败';
+
+        if(!empty($data)){
+            $a =new User();
+            $ret=$a->where(['mobile'=>$data['username'],'password'=>$data['password']])->count();
+            if($ret>0){
+                $code=200;
+                $msg='登录成功';
+            }
+        }
+        return json(array('code'=>$code,'msg'=>$msg));
+    }
 }
---
+++ application/index/model/User.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/8/17
+ * Time: 11:38
+ */
+namespace app\index\model;
+use think\Model;
+use think\db;
+use app\core\traits\InterfaceModel;
+
+class User extends Model {
+
+    protected $table = 'user';
+
+}
