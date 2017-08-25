a0a4374158a0f9201f762c0a564fded86e893e14
88659108 <88659108@qq.com>
2017-08-25 10:42:48 +0800

    文件地址

--- application/api/controller/Menu.php
+++ application/api/controller/Menu.php
@@ -70,27 +70,27 @@ class Menu
     {
         
         $goods_list = [];
-        $goods_list[] = ['id'=>1, 'name'=>'腐竹烧肉', 'img_url'=>'http://api.ai-life.me/Uploads/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>2];
-        $goods_list[] = ['id'=>2, 'name'=>'台湾卤肉', 'img_url'=>'http://api.ai-life.me/Uploads/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>2];
-        $goods_list[] = ['id'=>3, 'name'=>'梅菜扣肉', 'img_url'=>'http://api.ai-life.me/Uploads/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>2];
-        $goods_list[] = ['id'=>4, 'name'=>'红烧排骨', 'img_url'=>'http://api.ai-life.me/Uploads/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>2];
-        $goods_list[] = ['id'=>5, 'name'=>'土豆牛肉', 'img_url'=>'http://api.ai-life.me/Uploads/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>2];
-        $goods_list[] = ['id'=>6, 'name'=>'炭火烧肉饭(烧烤味)', 'img_url'=>'http://api.ai-life.me/Uploads/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>3];
-        $goods_list[] = ['id'=>7, 'name'=>'炭火烧肉饭(番茄味)', 'img_url'=>'http://api.ai-life.me/Uploads/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>3];
-        $goods_list[] = ['id'=>8, 'name'=>'炭火烧肉饭(黑胡椒)', 'img_url'=>'http://api.ai-life.me/Uploads/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>4];
-        $goods_list[] = ['id'=>9, 'name'=>'炭火烧肉饭(甜辣味)', 'img_url'=>'http://api.ai-life.me/Uploads/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>5];
-        $goods_list[] = ['id'=>10, 'name'=>'炭烧牛肉饭(甜辣味)', 'img_url'=>'http://api.ai-life.me/Uploads/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>5];
-        $goods_list[] = ['id'=>11, 'name'=>'炭烧牛肉饭(黑椒味)', 'img_url'=>'http://api.ai-life.me/Uploads/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>6];
-        $goods_list[] = ['id'=>12, 'name'=>'炭烧牛肉饭(番茄味)', 'img_url'=>'http://api.ai-life.me/Uploads/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>6];
-        $goods_list[] = ['id'=>13, 'name'=>'炭烧牛肉饭(烧烤味)', 'img_url'=>'http://api.ai-life.me/Uploads/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>7];
-        $goods_list[] = ['id'=>14, 'name'=>'炭烧培根饭(甜辣味)', 'img_url'=>'http://api.ai-life.me/Uploads/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>7];
+        $goods_list[] = ['id'=>1, 'name'=>'腐竹烧肉', 'img_url'=>'http://img.my-shop.cc/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>2];
+        $goods_list[] = ['id'=>2, 'name'=>'台湾卤肉', 'img_url'=>'http://img.my-shop.cc/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>2];
+        $goods_list[] = ['id'=>3, 'name'=>'梅菜扣肉', 'img_url'=>'http://img.my-shop.cc/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>2];
+        $goods_list[] = ['id'=>4, 'name'=>'红烧排骨', 'img_url'=>'http://img.my-shop.cc/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>2];
+        $goods_list[] = ['id'=>5, 'name'=>'土豆牛肉', 'img_url'=>'http://img.my-shop.cc/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>2];
+        $goods_list[] = ['id'=>6, 'name'=>'炭火烧肉饭(烧烤味)', 'img_url'=>'http://img.my-shop.cc/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>3];
+        $goods_list[] = ['id'=>7, 'name'=>'炭火烧肉饭(番茄味)', 'img_url'=>'http://img.my-shop.cc/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>3];
+        $goods_list[] = ['id'=>8, 'name'=>'炭火烧肉饭(黑胡椒)', 'img_url'=>'http://img.my-shop.cc/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>4];
+        $goods_list[] = ['id'=>9, 'name'=>'炭火烧肉饭(甜辣味)', 'img_url'=>'http://img.my-shop.cc/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>5];
+        $goods_list[] = ['id'=>10, 'name'=>'炭烧牛肉饭(甜辣味)', 'img_url'=>'http://img.my-shop.cc/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>5];
+        $goods_list[] = ['id'=>11, 'name'=>'炭烧牛肉饭(黑椒味)', 'img_url'=>'http://img.my-shop.cc/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>6];
+        $goods_list[] = ['id'=>12, 'name'=>'炭烧牛肉饭(番茄味)', 'img_url'=>'http://img.my-shop.cc/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>6];
+        $goods_list[] = ['id'=>13, 'name'=>'炭烧牛肉饭(烧烤味)', 'img_url'=>'http://img.my-shop.cc/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>7];
+        $goods_list[] = ['id'=>14, 'name'=>'炭烧培根饭(甜辣味)', 'img_url'=>'http://img.my-shop.cc/goods/new1.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>7];
 
 
 
-        $goods_list[] = ['id'=>15, 'name'=>'绿豆粥', 'img_url'=>'http://api.ai-life.me/Uploads/goods/tang5.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>8];
-        $goods_list[] = ['id'=>16, 'name'=>'红枣银耳羹', 'img_url'=>'http://api.ai-life.me/Uploads/goods/tang6.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>8];
-        $goods_list[] = ['id'=>17, 'name'=>'皮蛋粥', 'img_url'=>'http://api.ai-life.me/Uploads/goods/tang7.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>8];
-        $goods_list[] = ['id'=>18, 'name'=>'红豆粥', 'img_url'=>'http://api.ai-life.me/Uploads/goods/tang8.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>8];
+        $goods_list[] = ['id'=>15, 'name'=>'绿豆粥', 'img_url'=>'http://img.my-shop.cc/goods/tang5.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>8];
+        $goods_list[] = ['id'=>16, 'name'=>'红枣银耳羹', 'img_url'=>'http://img.my-shop.cc/goods/tang6.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>8];
+        $goods_list[] = ['id'=>17, 'name'=>'皮蛋粥', 'img_url'=>'http://img.my-shop.cc/goods/tang7.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>8];
+        $goods_list[] = ['id'=>18, 'name'=>'红豆粥', 'img_url'=>'http://img.my-shop.cc/goods/tang8.jpg', 'price'=>28.00, 'stars'=>5, 'cate_id'=>8];
 
 
         return jsonData(1, 'ok', ['goods_list'=>$goods_list]);
