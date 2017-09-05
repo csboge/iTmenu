826d7fadfb267e561d3b9a9e803123e3586f153d
88659108 <88659108@qq.com>
2017-09-05 16:43:00 +0800

    df

--- config/database.php
+++ config/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '192.168.1.175',
     // 数据库名
     'database'        => 'drill',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'aidblife1073',
+    'password'        => 'root',
     // 端口
     'hostport'        => '',
     // 连接dsn
