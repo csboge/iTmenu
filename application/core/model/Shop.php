d130f457c28942841981f18d500351289ec49193
pengyangchao <474114736@qq.com>
2017-09-12 17:51:43 +0800

    s

---
+++ application/core/model/Shop.php
@@ -0,0 +1,26 @@
+<?php
+namespace app\core\model;
+
+use think\Model;
+use think\db;
+
+/**
+ * 订单方面 * 模型 * 操作方法
+ *
+ *
+ *
+ */
+class Shop extends Model
+{
+
+
+    /**
+     * 查询订单（订单号）
+     *
+     * @param   string   $ordersn    订单号
+     *
+     * @return   array    查到的订单
+     *
+     */
+
+}
