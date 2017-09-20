b476e3077ce024d1aaa51bb6ab3115017b8f67e4
pengyangchao <474114736@qq.com>
2017-09-20 15:44:13 +0800

    a

--- application/core/provider/WeChat.php
+++ application/core/provider/WeChat.php
@@ -133,6 +133,7 @@ class WeChat
     public function code($shop_id,$bagid){
 
         $session        = $this->p_auth->session();
+        return $session;
         $date = json_encode($session);
         my_log('orders',$date,'wechat/code',0,'新用户验证不通过');
         $access_token = $session['access_token'];
