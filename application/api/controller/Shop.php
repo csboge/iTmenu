912c9f029adab78285756fac700cff98ab40a3dc
pengyangchao <474114736@qq.com>
2017-09-20 11:07:20 +0800

    a

--- application/api/controller/Shop.php
+++ application/api/controller/Shop.php
@@ -44,7 +44,7 @@ class Shop
         ];
         $data = Db::name('shop')
             ->where($map)
-            ->field('title,logo,mobile,tel,video,shop_hours,notice,is_first,adress,lng,Lat,stations')
+            ->field('title,logo,mobile,tel,video,shop_hours,notice,is_first,adress,lng,Lat,stations,package')
             ->find();
         if($data){
             $data['logo'] = ImgUrl($data['logo']);
