7cce9086c8bb4e91bfee5cad76216d2e5f6415d5
pengyangchao <474114736@qq.com>
2017-10-14 17:01:52 +0800

    a

--- application/api/controller/Lecturer.php
+++ application/api/controller/Lecturer.php
@@ -189,9 +189,85 @@ class Lecturer
                 ],
 
             ],
+            4 => [
+                'name'          => '肖茂峰',
+                'explain'       => '好望角高级内训师',
+                'portrait'      => GET_IMG_URL.'lecturer/xmf/portrait.jpg',
+                'phone'         => '15956540455',
+                'bg'            => GET_IMG_URL.'lecturer/xmf/bg.jpg',
+                'deeds'         => [
+                    1               => [
+                        'title'         => '2009年',
+                        'content'       => '流水线工厂'
+                    ],
+                    2               => [
+                        'title'         => '2012年',
+                        'content'       => '送货员'
+                    ],
+                    3               => [
+                        'title'         => '2014年',
+                        'content'       => '饭店服务员'
+                    ],
+                    4               => [
+                        'title'         => '2015年',
+                        'content'       => '外资企业'
+                    ],
+                    5               => [
+                        'title'         => '2017年',
+                        'content'       => '好望角导师'
+                    ]
+                ],
+                'product'        => [
+                    1               => [
+                        'img'           => GET_IMG_URL.'lecturer/xmf/product/1.jpg',
+                        'title'         => '中国青年创业导师'
+                    ],
+                    2               => [
+                        'img'           => GET_IMG_URL.'lecturer/xmf/product/2.jpg',
+                        'title'         => '好望角商学院金牌讲师'
+                    ],
+                    3               => [
+                        'img'           => GET_IMG_URL.'lecturer/xmf/product/3.jpg',
+                        'title'         => '好望角高级内训师'
+                    ],
+                    4               => [
+                        'img'           => GET_IMG_URL.'lecturer/xmf/product/4.jpg',
+                        'title'         => '好望角人性商道研究员'
+                    ]
+
+                ],
+                'join'          => [
+                    '曾经因为一场演讲点亮了自己的生命',
+                    '明白了帮助别人实现梦想点亮他人生命才是人生的真谛',
+                    '讲师站上舞台就是帮助别人实现更大价值',
+                    '而销售人员的天职就是持续不断地为客户创造价值'
+                ],
+                'banner'          => [
+                    GET_IMG_URL.'lecturer/zl/banner/1.jpg',
+                    GET_IMG_URL.'lecturer/zl/banner/2.jpg',
+                    GET_IMG_URL.'lecturer/zl/banner/3.jpg',
+                    GET_IMG_URL.'lecturer/zl/banner/4.jpg'
+                ],
+                'image'          => [
+                    GET_IMG_URL.'lecturer/zl/photo/1.jpg',
+                    GET_IMG_URL.'lecturer/zl/photo/2.jpg',
+                    GET_IMG_URL.'lecturer/zl/photo/3.jpg',
+                    GET_IMG_URL.'lecturer/zl/photo/4.jpg',
+                    GET_IMG_URL.'lecturer/zl/photo/5.jpg',
+                    GET_IMG_URL.'lecturer/zl/photo/6.jpg',
+                    GET_IMG_URL.'lecturer/zl/photo/7.jpg',
+                    GET_IMG_URL.'lecturer/zl/photo/8.jpg',
+                    GET_IMG_URL.'lecturer/zl/photo/9.jpg',
+                    GET_IMG_URL.'lecturer/zl/photo/10.jpg',
+                ],
+
+            ],
+
         ];
         return jsonData(1, 'ok', $data[$id]);
     }
+
+
 
     public function details(){
         $id = input('param.id/d');
