f1f4d914047a126d2e15db9b63a4d8641e77e0a6
pengyangchao <474114736@qq.com>
2017-10-19 10:50:05 +0800

    a

--- application/api/provider/Zlf.php
+++ application/api/provider/Zlf.php
@@ -23,7 +23,7 @@ class Zlf
     {
         $data = [
             'name'          => '萧鼎诚',
-            'explain'       => '亚洲90后超级演说家',
+            'explain'       => '亚洲90后首席创业导师',
             'portrait'      => GET_IMG_URL.'lecturer/zlf/portrait.jpg?'.GET_RAND,
             'phone'         => '18671621319',
             'banner'        => [
@@ -36,28 +36,6 @@ class Zlf
                 'banner7'       => GET_IMG_URL.'lecturer/zlf/3.jpg?'.GET_RAND,
                 'banner8'       => GET_IMG_URL.'lecturer/zlf/4.jpg?'.GET_RAND,
             ],
-            'deeds'         => [
-                1               => [
-                    'title'         => '亚洲90后超级演说家',
-                    'content'       => '22岁打破集团9年来的销售记录',
-                    'colour'        => '#e0620c'
-                ],
-                2               => [
-                    'title'         => '好望角集团浙江分公司总经理',
-                    'content'       => '23岁成为浙江分公司总经理',
-                    'colour'        => '#00a0e9'
-                ],
-                3               => [
-                    'title'         => '好望角商学院首席创业导师',
-                    'content'       => '24岁仅用九个月成功挑战开上宝马',
-                    'colour'        => '#35b16c'
-                ],
-                4               => [
-                    'title'         => '好望角集团董事',
-                    'content'       => '27岁成为集团董事帮助团队伙伴开上奔驰宝马',
-                    'colour'        => '#959595'
-                ]
-            ]
         ];
 
         //返回内容
