fd98490515eb22428a7d123275397f7866fbe3e0
北 <315584304@qq.com>
2017-11-09 17:04:27 +0800

    分类

--- application/index/model/TypeGoods.php
+++ application/index/model/TypeGoods.php
@@ -10,8 +10,11 @@ use think\Model;
 class TypeGoods extends Model
 {
     //查找菜品类别名称
-    public function typeId(){
-        $name = $this->select();
+    public function typeId($shop_id){
+        $map = [
+            'shop_id' => $shop_id
+        ];
+        $name = $this->where($map)->select();
 
         return json_decode(json_encode($name), true);
     }
